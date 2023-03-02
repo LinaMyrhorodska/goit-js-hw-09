3626f99f0cd1fb906481201ea568ca911d4e5576
LinaMyrhorodska <lmyrhorodska2511@gmail.com>
2023-03-02 16:51:09 +0200

    hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,18 +6,28 @@ const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 
 let timerId = null;
+let isActive = false;
 
 startButton.addEventListener('click', onButtonStart);
 stopButton.addEventListener('click', onButtonStop);
 
 function onButtonStart() {
+    if (isActive) {
+        return;
+    };
+
+    
+
     timerId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
         startButton.setAttribute('disabled', 'disabled');
     }, 1000);
+
+    isActive = true;
 }
 
 function onButtonStop() {
     clearInterval(timerId);
-    startButton.removeAttribute('disabled', 'disabled')
+    startButton.removeAttribute('disabled', 'disabled');
+    isActive = false;
 }
